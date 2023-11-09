4b470841c8dd51906360156651adf3ab089656b6
Nicolly Damasceno <rm87077@estudante.fieb.edu.br>
2023-11-09 07:53:15 -0300

    tudo ok mana

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+
+    Pessoa qualquer = new Pessoa();
+    qualquer.setNome("Julia Bonner");
+    System.out.println(qualquer.getNome());
+}
+}
